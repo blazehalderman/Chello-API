003d00378242b4e4169eafdf67d15a41aad97612
Kyle Nosar <kylenosar@gmail.com>
2018-05-24 15:02:12 -0700

    initial commit

---
+++ server/server.js
@@ -0,0 +1,9 @@
+require('./config/config');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
